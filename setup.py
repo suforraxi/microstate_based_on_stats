50e3189a4c30092bdbdecf50cd50aa3653813084
matte <suforraxi@gmail.com>
2025-12-12 16:17:31 +0100

    fixed setup.py file for package input

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
     install_requires=[
                 "numpy"
                 "setuptools",
-                "os",
                 "pandas",
                 "matplotlib",
                 "scipy",
