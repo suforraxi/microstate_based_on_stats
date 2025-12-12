c71c8daec970de422c916bcf27667dfeaa5f0faf
matte <suforraxi@gmail.com>
2025-12-12 16:15:07 +0100

    moving inside the input directory for the package

---
+++ micros_based_stats/__init__.py
@@ -0,0 +1,9 @@
+# This file marks the directory as a Python package.
+# You can import your module files here if needed.
+# Example:
+# from .module_name import some_function
+
+from .microstate_analysis import *
+from .aggregate_peak_backfits import *
+from .plot_stat_per_k import *
+from .plot_utils import *
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="microstate_based_on_stats",
+    version="0.0.1",
+    author="Matteo Demuru",
+    author_email="suforraxi@gmail.com",
+    description="A package for selecting microstate classes based on statistics.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/suforraxi/microstate_based_on_stats",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+                "numpy"
+                "setuptools",
+                "os",
+                "pandas",
+                "matplotlib",
+                "scipy",
+                "sklearn",
+                "statsmodels",
+                "mne",
+                "glob",
+                "neurokit2"
+    ]
+)
