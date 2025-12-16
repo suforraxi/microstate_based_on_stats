ae6f9dd2140dae510e863c0504ceb7f5ea9d147f
matte <suforraxi@gmail.com>
2025-12-16 14:28:14 +0100

    version modified 0.0.2

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="micros_based_on_stats",
-    version="0.0.1",
-    author="Matteo Demuru",
-    author_email="suforraxi@gmail.com",
-    description="A package for selecting microstate classes based on statistics.",
+    name="micros_based_stats",  # Replace with your package name
+    version="0.0.2",  # Package version
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package for microstate analysis based on statistics",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/suforraxi/microstate_based_on_stats",
-    packages=find_packages(),
+    url="https://github.com/yourusername/microstate_based_on_stats",  # Replace with your repo URL
+    packages=find_packages(),  # Automatically find sub-packages
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "statsmodels",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
-    install_requires=[
-                "numpy",
-                "setuptools",
-                "pandas",
-                "matplotlib",
-                "scipy",
-                "scikit-learn",
-                "statsmodels",
-                "mne",
-                "neurokit2"
-    ]
+    python_requires=">=3.6",  # Minimum Python version
 )
